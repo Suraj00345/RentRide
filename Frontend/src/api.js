25875616883e911073e64281604b7711b335f12e
Suraj00345 <surajkumarmanna2000@gmail.com>
2026-04-07 23:11:20 +0530

    add real server API URL

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,4 +1,4 @@
-const VITE_API_URL = "https://rentride-es60.onrender.com";
+const VITE_API_URL = "https://rentride-vn2n.onrender.com";
 // const VITE_API_URL = "http://localhost:3000";
 
 export default VITE_API_URL;
